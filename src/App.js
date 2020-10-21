ae0cd788961167b4a84fc61c155d6f656f813fb9
kimhan0421 <kimhan0421@naver.com>
2020-10-21 23:52:37 +0900

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import notify from './notify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    SplitMe: null
+  }
+  handleClick = async () => {
+    const loadeModule = await import('./SplitMe');
+    this.setState({
+      SplitMe: loadeModule.defulat
+    })
+  }
+  render(){
+    const {SplitMe} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">x
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p onClick = {this.handleClick}>Hello React!</p>
+          {SplitMe &&<SplitMe/>}
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
